022d15df20e2d755af59a5486dac07c3330fdd24
AdiDD <adi.deaconu21@gmail.com>
2021-12-14 10:58:57 +0200

    Remove useEffect from PrivateRoute

--- src/app/layout/PrivateRoute.js
+++ src/app/layout/PrivateRoute.js
@@ -1,12 +1,10 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import { Redirect, Route } from "react-router-dom";
 import UserContext from "../context/user-context"
 
 function PrivateRoute({ children, ...rest }) {
     const userCtx = useContext(UserContext);
-    useEffect(() => {
-        console.log(userCtx.user);
-    }, [])
+    
     return (
         <Route
             {...rest}
