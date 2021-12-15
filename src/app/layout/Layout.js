5eca70b65e2ad93ede84edda7954dee937b9f837
AdiDD <adi.deaconu21@gmail.com>
2021-12-16 01:12:07 +0200

    Make automatic breadcrumbs

--- src/app/layout/Layout.js
+++ src/app/layout/Layout.js
@@ -7,35 +7,55 @@ import { Link } from "react-router-dom";
 
 const Layout = ({ children }) => {
     useEffect(()  => {
+        addBodyClasses();
+        
+        return () => removeBodyClasses();
+    });
+
+    const addBodyClasses = () => {
         document.body.classList.add('header-fixed');
         document.body.classList.add('header-tablet-and-mobile-fixed');
         document.body.classList.add('toolbar-enabled');
         document.body.style.backgroundImage = `url(${BackgroundPattern})`
+    };
 
-        return () => {
-            document.body.classList.remove('header-fixed');
-            document.body.classList.remove('header-tablet-and-mobile-fixed');
-            document.body.classList.remove('toolbar-enabled');
-            document.body.style.backgroundImage = 'initial';
-        };
-    });
+    const removeBodyClasses = () => {
+        document.body.classList.remove('header-fixed');
+        document.body.classList.remove('header-tablet-and-mobile-fixed');
+        document.body.classList.remove('toolbar-enabled');
+        document.body.style.backgroundImage = 'initial';
+    };
+
+    const generateBreadcrumbs = () => {
+        const here = window.location.href.split('/').slice(3);
+        // console.log(here)
+        const parts = [];
+        // console.log(parts)
+
+        function capitalize(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+    
+        for (let i = 0; i < here.length; i++) {
+            const part = here[i];
+            // console.log(part)
+            const text = capitalize(decodeURIComponent(part).toLowerCase().split('.')[0]);
+            // console.log(text)
+            const link = '/' + here.slice(0, i + 1).join('/');
+            // console.log(link)
+            if (text !== "")
+                parts.push({"title": text, "to": link});
+        }
+
+        // console.log(parts)
+        return parts
+    }
+
+    const links = generateBreadcrumbs();
 
     const breadcrumbsOptions = {
-        title: "Breadcrumbs(pending...)",
-        links: [
-            {
-                to: "/cars",
-                title: "Car List"
-            },
-            {
-                to: "/cars/1",
-                title: "Toyota Camry/Vista x30"
-            },
-            {
-                to: "/cars/1/history",
-                title: "History"
-            }
-        ]
+        title: links.length > 0 ? links[links.length - 1].title : "Home",
+        links
     }
 
     return (
@@ -80,7 +100,7 @@ const Layout = ({ children }) => {
 						{/* end::Container */}
 					</div>
 					
-            		<Breadcrumbs options={breadcrumbsOptions}/>
+            		{links.length > 0 && <Breadcrumbs options={breadcrumbsOptions}/>}
                     {children}
 
                 </div>
