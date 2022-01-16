562896f6cc31310512db1be72f0571c8f7ce3ebd
red-victor <77648244+red-victor@users.noreply.github.com>
2022-01-16 11:30:28 +0200

    Add more details to MySolvedHistory Item

--- src/app/features/home/tabs/overview/history/SolvedHistoryItem.js
+++ src/app/features/home/tabs/overview/history/SolvedHistoryItem.js
@@ -1,8 +1,31 @@
+import { Link } from "react-router-dom";
 import PlacholderImage from "../../../../../../assets/img/solved-history-placeholder.jpg";
 
 const SolvedHistoryItem = (props) => {
     const { historyItem } = props;
     console.log(historyItem);
+
+    const serviceType = (serviceType) => {
+        switch (serviceType) {
+            case 0: return 'RCA';
+            case 1: return 'CASCO';
+            case 2: return 'ITP';
+            case 3: return 'Revision';
+            case 4: return 'Consumable';
+            default: return 'Other';
+        }
+    }
+
+    const serviceTypeTextColor = (serviceType) => {
+        switch (serviceType) {
+            case 0:
+            case 1: return 'primary';
+            case 2: return 'warning';
+            case 3: return 'info';
+            case 4: return 'success';
+            default: return 'danger';
+        }
+    }
 
     return (
         <>
@@ -23,17 +46,14 @@ const SolvedHistoryItem = (props) => {
                     <span className="text-dark fw-bolder d-block fs-5">{historyItem.cost}€</span>
                 </td>
                 <td className="text-end">
-                    <span className="text-primary fs-7 fw-bolder">+28%</span>
+                    <span className={"text-" + serviceTypeTextColor(historyItem.serviceType) + " fs-7 fw-bolder"}>{serviceType(historyItem.serviceType)}</span>
                 </td>
                 <td className="text-end">
-                    <a href="#" className="btn btn-sm btn-icon btn-bg-light btn-active-color-primary">
-                        <span className="svg-icon svg-icon-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
-                                <rect opacity="0.5" x={18} y={13} width={13} height={2} rx={1} transform="rotate(-180 18 13)" fill="black" />
-                                <path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="black" />
-                            </svg>
-                        </span>
-                    </a>
+                    {/* <div className="btn btn-sm btn-icon btn-bg-light btn-active-color-primary"> */}
+                    <Link to={`/cars/${historyItem.carId}`} className="btn btn-sm btn-icon btn-bg-light btn-active-color-primary">
+                        To Car
+                    </Link>
+                    {/* </div> */}
                 </td>
             </tr>
 
