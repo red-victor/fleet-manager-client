8b7f38e3f09d6f1143fa4999893bf41fdd9d1c35
AdiDD <adi.deaconu21@gmail.com>
2022-01-22 09:52:03 +0200

    Add changeEmail route to api agent

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -57,6 +57,7 @@ const Account = {
     changeMyPassword: payload => requests.put("account/change-my-password", payload),
     sendResetPasswordLink: payload => requests.post("account/request-reset-password", payload),
     resetPassword: payload => requests.post("account/reset-password", payload),
+    changeEmail: payload => requests.put("account/change-email", payload),
 }
 
 const Users = {
