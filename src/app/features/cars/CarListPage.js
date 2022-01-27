3ea28f1df7d15e6b4288e4c61831c48e8a94a1fd
AdiDD <adi.deaconu21@gmail.com>
2022-01-27 12:52:59 +0200

    Fix CarListPage merge errors

--- src/app/features/cars/CarListPage.js
+++ src/app/features/cars/CarListPage.js
@@ -2,13 +2,10 @@ import CarList from "./list/CarList";
 import { useState, useEffect } from "react"
 import agent from "../../api/agent";
 import AddCarModal from "./modal/AddCarModal";
-<<<<<<< HEAD
 import CarSearchInput from "./list/CarSearchInput";
 import CarFilterButton from "./list/CarFilterButton";
 import AddCarButton from "./list/AddCarButton";
-=======
 import UploadFileModal from "../../layout/appComponents/UploadFileModal";
->>>>>>> 1ab565cb6cdcd2f5677cf3bdcfd42f2c67e0f7a0
 
 const CarListPage = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -43,7 +40,6 @@ const CarListPage = () => {
             .finally(() => setIsSubmitting(false));
     }
 
-<<<<<<< HEAD
     const searchCar = name => {        
         if (name.trim() === "") {
             getData();
@@ -55,7 +51,8 @@ const CarListPage = () => {
             .then(cars => setCars(cars))
             .catch(e => console.log(e))
             .finally(() => setIsLoading(false));
-=======
+    }
+
     const uploadFile = formData => {
         setIsSubmitting(true);
         console.log(formData.get('File'));
@@ -66,7 +63,6 @@ const CarListPage = () => {
             })
             .catch(e => console.log(e))
             .finally(() => setIsSubmitting(false));
->>>>>>> 1ab565cb6cdcd2f5677cf3bdcfd42f2c67e0f7a0
     }
 
     return (
@@ -79,66 +75,8 @@ const CarListPage = () => {
                         </div>
                         <div className="card-toolbar">
                             <div className="d-flex justify-content-end" data-kt-subscription-table-toolbar="base" >
-<<<<<<< HEAD
                                 <CarFilterButton />
                                 <AddCarButton handleShowAddCarModal={handleShowAddCarModal} />
-=======
-                                <button
-                                    type="button"
-                                    className="btn btn-light-primary me-3"
-                                    data-kt-menu-trigger="click"
-                                    data-kt-menu-placement="bottom-end"
-                                >
-                                    <span className="svg-icon svg-icon-2">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width={24}
-                                            height={24}
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                        >
-                                            <path
-                                                d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z"
-                                                fill="black"
-                                            />
-                                        </svg>
-                                    </span>
-                                    Filter
-                                </button>
-                                <button
-                                    className="btn btn-primary er fs-6 px-8 py-4"
-                                    onClick={handleShowAddCarModal}
-                                >
-                                    <span className="svg-icon svg-icon-2">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width={24}
-                                            height={24}
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                        >
-                                            <rect
-                                                opacity="0.5"
-                                                x="11.364"
-                                                y="20.364"
-                                                width={16}
-                                                height={2}
-                                                rx={1}
-                                                transform="rotate(-90 11.364 20.364)"
-                                                fill="black"
-                                            />
-                                            <rect
-                                                x="4.36396"
-                                                y="11.364"
-                                                width={16}
-                                                height={2}
-                                                rx={1}
-                                                fill="black"
-                                            />
-                                        </svg>
-                                    </span>
-                                    Add Car
-                                </button>
 
                                 <button type="button"
                                     className="btn btn-primary"
@@ -153,8 +91,6 @@ const CarListPage = () => {
                                     </span>
                                     Upload Excel
                                 </button>
-
->>>>>>> 1ab565cb6cdcd2f5677cf3bdcfd42f2c67e0f7a0
                             </div>
                         </div>
                     </div>
