290daef2d001af823dddb1afbe334c1c5727324e
AdiDD <adi.deaconu21@gmail.com>
2022-02-01 11:50:00 +0200

    Make all user and car list buttons primary buttons

--- src/app/features/cars/list/CarFilterButton.js
+++ src/app/features/cars/list/CarFilterButton.js
@@ -4,7 +4,7 @@ const CarFilterButton = () => {
     return (
         <button
             type="button"
-            className="btn btn-light-primary me-3"
+            className="btn btn-primary me-3"
             data-kt-menu-trigger="click"
             data-kt-menu-placement="bottom-end"
         >
