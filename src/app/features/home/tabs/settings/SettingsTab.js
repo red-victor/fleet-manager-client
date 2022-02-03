2235eb4a6c1413527da927d2d7c1e0d1c712db6a
AdiDD <adi.deaconu21@gmail.com>
2022-02-03 12:30:59 +0200

    Remove Email preferences from settings tab

--- src/app/features/home/tabs/settings/SettingsTab.js
+++ src/app/features/home/tabs/settings/SettingsTab.js
@@ -4,39 +4,6 @@ const SettingsTab = () => {
     return (
         <>
             <SignInMethod />
-            <div className="card mb-5 mb-xl-10">
-                <div className="card-header border-0 cursor-pointer" role="button" data-bs-toggle="collapse" data-bs-target="#kt_account_email_preferences" aria-expanded="true" aria-controls="kt_account_email_preferences">
-                    <div className="card-title m-0">
-                        <h3 className="fw-bolder m-0">Email Preferences</h3>
-                    </div>
-                </div>
-                <div id="kt_account_settings_email_preferences" className="collapse show">
-                    <form className="form">
-                        <div className="card-body border-top px-9 py-9">
-                            <label className="form-check form-check-custom form-check-solid align-items-start">
-                                <input className="form-check-input me-3" type="checkbox" name="email-preferences[]" defaultValue={1} />
-                                <span className="form-check-label d-flex flex-column align-items-start">
-                                    <span className="fw-bolder fs-5 mb-0">Successful Payments</span>
-                                    <span className="text-muted fs-6">Receive a notification for every successful payment.</span>
-                                </span>
-                            </label>
-                            <div className="separator separator-dashed my-6" />
-                            <label className="form-check form-check-custom form-check-solid align-items-start">
-                                <input className="form-check-input me-3" type="checkbox" name="email-preferences[]" defaultChecked="checked" defaultValue={1} />
-                                <span className="form-check-label d-flex flex-column align-items-start">
-                                    <span className="fw-bolder fs-5 mb-0">Payouts</span>
-                                    <span className="text-muted fs-6">Receive a notification for every initiated payout.</span>
-                                </span>
-                            </label>
-                            <div className="separator separator-dashed my-6" />
-                        </div>
-                        <div className="card-footer d-flex justify-content-end py-6 px-9">
-                            <button className="btn btn-light btn-active-light-primary me-2">Discard</button>
-                            <button className="btn btn-primary px-6">Save Changes</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
         </>
     );
 }
